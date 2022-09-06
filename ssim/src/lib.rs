2199112643690caf9efa5d19f1b18514b87d33fb
infcode <infcode@umich.edu>
2022-09-06 16:43:48 -0400

    evaluate intermediate variables for ssim

--- ssim/src/lib.rs
+++ ssim/src/lib.rs
@@ -1,12 +1,17 @@
-use opencv::core::{Size, BORDER_DEFAULT};
-use opencv::{imgproc::gaussian_blur, prelude::*};
+/**
+ * This file is an rust version of an opencv based SSIM impliment in C++:
+ * https://github.com/jrmuizel/ssim/blob/master/SSIM.cpp
+ */
+use opencv::{core as cvcore, Error};
+use opencv::{imgproc, prelude::*};
 
 // calculate the SSIM value comparing 2 grayscale image
-fn ssim(image1: &[&[u8]], image2: &[&[u8]]) -> f32 {
+pub fn ssim(image1: &[&[u8]], image2: &[&[u8]]) -> Result<f32, Error> {
     // 1. Initialize all required variables
     // The initial images
     let img1 = Mat::from_slice_2d(image1).unwrap();
     let img2 = Mat::from_slice_2d(image2).unwrap();
+    let size = img1.size().unwrap();
 
     // pixel-wise product of images (intermediate variables)
     let mut img11 = Mat::default();
@@ -22,6 +27,9 @@ fn ssim(image1: &[&[u8]], image2: &[&[u8]]) -> f32 {
     let mut mu22 = Mat::default();
     let mut mu12 = Mat::default();
 
+    // temporary variable for evaluating sigma
+    let mut sigma_tmp = Mat::default();
+
     // the squared variances of images
     let mut sigma11 = Mat::default();
     let mut sigma22 = Mat::default();
@@ -29,11 +37,63 @@ fn ssim(image1: &[&[u8]], image2: &[&[u8]]) -> f32 {
     // the covariance of images
     let mut sigma12 = Mat::default();
 
-    // 2. Calculating all these variables
+    // 2. Calculating all variables above
     // TODO
-    gaussian_blur(&img1, &mut mu1, Size::new(11, 11), 1.5, 1.5, BORDER_DEFAULT).unwrap();
-    gaussian_blur(&img2, &mut mu2, Size::new(11, 11), 1.5, 1.5, BORDER_DEFAULT).unwrap();
+    cvcore::pow(&img1, 2f64, &mut img11)?;
+    cvcore::pow(&img2, 2f64, &mut img22)?;
+    cvcore::mul_mat_mat(&img1, &img2)?.to_mat()?;
+
+    imgproc::gaussian_blur(
+        &img1,
+        &mut mu1,
+        cvcore::Size::new(11, 11),
+        1.5,
+        1.5,
+        cvcore::BORDER_DEFAULT,
+    )?;
+    imgproc::gaussian_blur(
+        &img2,
+        &mut mu2,
+        cvcore::Size::new(11, 11),
+        1.5,
+        1.5,
+        cvcore::BORDER_DEFAULT,
+    )?;
+
+    cvcore::pow(&mu1, 2f64, &mut mu11)?;
+    cvcore::pow(&mu2, 2f64, &mut mu22)?;
+    cvcore::mul_mat_mat(&mu1, &mu2)?.to_mat()?;
+
+    imgproc::gaussian_blur(
+        &img11,
+        &mut sigma_tmp,
+        cvcore::Size::new(11, 11),
+        1.5,
+        1.5,
+        cvcore::BORDER_DEFAULT,
+    )?;
+    cvcore::add_weighted(&sigma_tmp, 1f64, &mu11, -1f64, 0f64, &mut sigma11, -1)?;
+
+    imgproc::gaussian_blur(
+        &img22,
+        &mut sigma_tmp,
+        cvcore::Size::new(11, 11),
+        1.5,
+        1.5,
+        cvcore::BORDER_DEFAULT,
+    )?;
+    cvcore::add_weighted(&sigma_tmp, 1f64, &mu22, -1f64, 0f64, &mut sigma22, -1)?;
+
+    imgproc::gaussian_blur(
+        &img12,
+        &mut sigma_tmp,
+        cvcore::Size::new(11, 11),
+        1.5,
+        1.5,
+        cvcore::BORDER_DEFAULT,
+    )?;
+    cvcore::add_weighted(&sigma_tmp, 1f64, &mu12, -1f64, 0f64, &mut sigma12, -1)?;
     // 3. The formula of SSIM
     // TODO
-    0f32
+    Ok(0f32)
 }
